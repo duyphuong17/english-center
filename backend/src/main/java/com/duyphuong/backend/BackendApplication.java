fec73fe558f7398e85dc0412ec86154e581c1eb9
duy phuong <duyphuong172004@gmail.com>
2026-01-28 15:34:20 +0700

    khởi tạo dự án

---
+++ backend/src/main/java/com/duyphuong/backend/BackendApplication.java
@@ -0,0 +1,18 @@
+package com.duyphuong.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+// @SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
